0fa838c0132274c3411dfbb7672a8145b5d63164
devansh888 <84080583+devansh888@users.noreply.github.com>
2021-08-13 14:19:32 +0530

    Add files via upload

---
+++ asd1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<string.h>
+
+void hello(void)
+{
+    printf("hello");
+}
+
+int main(void)
+{
+    for(int i = 0; i<3; i++)
+    {
+        printf("hello\n");
+    }
+}
